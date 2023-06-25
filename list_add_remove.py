9187bd748ddddb72f95677e35fea4df165879349
Rownita <47550779+Rownita@users.noreply.github.com>
2023-06-25 00:01:53 -0700

    Update list_add_remove.py

--- list_add_remove.py
+++ list_add_remove.py
@@ -1,6 +1,6 @@
-### Please write a program which asks the user to choose between addition and removal. Depending on the choice, the program adds an item to or removes an item from the end of a list. The item that is added must always be one greater than the last item in the list. The first item to be added must be 1.
+### Please write a program that asks the user to choose between addition and removal. Depending on the choice, the program adds an item to or removes an item from the end of a list. The item that is added must always be one greater than the last item in the list. The first item to be added must be 1.
 
-### The list is printed out in the beginning and after each operation. Have a look at the example execution below:
+### The list is printed out at the beginning and after each operation. Have a look at the example execution below:
 
 ###Sample output
 ###The list is now []
@@ -25,7 +25,7 @@ i = 1
 while True:
 
     a = input(f"a(d)d, (r)emove or e(x)it:")
-    if a == 'd':
+    if a == 'd':  ### Conditions for adding
         if len(list) != 0:
             list.append(i)
             i = i + 1
@@ -36,11 +36,11 @@ while True:
             i = i + 1
             print(f"The list is now {list}")
 
-    if a == 'r':
+    if a == 'r':  ### conditions for removing
         list.pop()
         i = i-1
         print(f"The list is now {list}")
 
-    if a == 'x':
+    if a == 'x':  ### condition for exit
         print("Bye!")
         break
