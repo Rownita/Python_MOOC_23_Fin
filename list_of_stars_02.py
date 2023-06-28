fcb97dccda792a10330b1aab3f9bac407202b39a
Rownita <47550779+Rownita@users.noreply.github.com>
2023-06-28 03:26:34 -0700

    Create list_of_stars_02.py

---
+++ list_of_stars_02.py
@@ -0,0 +1,18 @@
+"""
+Please write a function named list_of_stars, which takes a list of integers as its argument. The function should print out lines of star characters. The numbers in the list specify how many stars each line should contain.
+
+For example, with the function call list_of_stars([3, 7, 1, 1, 2]) the following should be printed out:
+
+"""
+
+### Write your solution here
+
+def list_of_stars(my_list):
+    for i in my_list:
+        for j in range(1,i+1,1):
+            print("*",end="")
+        print("")
+
+
+if __name__ == "__main__":
+    list_of_stars([3,7,1,1,2])
